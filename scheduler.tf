995524fccbd02952537a69728d834a5caba6e2b2
Stelagowski Tomasz <stelagowski.tomasz@bcg.com>
2022-03-12 00:59:38 +0100

    Add schedulre for timeout and rejection

--- scheduler.tf
+++ scheduler.tf
@@ -5,7 +5,7 @@ resource "google_cloud_scheduler_job" "gcf-matches-create-trigger" {
 
   pubsub_target {
     topic_name = module.gcf_matches-create.pubsub_topic_id
-    data       = base64encode("{}}")
+    data       = base64encode("{}")
   }
 }
 
@@ -27,7 +27,28 @@ resource "google_cloud_scheduler_job" "gcf_matches-create-offering-notifications
 
   pubsub_target {
     topic_name = module.gcf_matches-create-offering-notifications.pubsub_topic_id
-    data       = base64encode("{}}")
+    data       = base64encode("{}")
   }
 }
 
+resource "google_cloud_scheduler_job" "gcf_matches-process-timeout-trigger" {
+  name        = "gcf_matches-process-timeout-trigger"
+  description = "gcf_matches-process-timeout-trigger"
+  schedule    = "20 */5 * * *" # every 3 hours
+
+  pubsub_target {
+    topic_name = module.gcf_matches_process_timeout.pubsub_topic_id
+    data       = base64encode("{}")
+  }
+}
+
+resource "google_cloud_scheduler_job" "gcf_matches-process-rejections-trigger" {
+  name        = "gcf_matches-process-rejections-trigger"
+  description = "gcf_matches-process-rejections-trigger"
+  schedule    = "20 */5 * * *" # every 3 hours
+
+  pubsub_target {
+    topic_name = module.gcf_matches_process_rejections.pubsub_topic_id
+    data       = base64encode("{}")
+  }
+}
