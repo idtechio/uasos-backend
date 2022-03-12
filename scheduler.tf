250553a8d04b55370ce5aa3e560bc789917c9e05
Stelagowski Tomasz <stelagowski.tomasz@bcg.com>
2022-03-13 00:07:07 +0100

    Disable chrons

--- scheduler.tf
+++ scheduler.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_scheduler_job" "gcf-matches-create-trigger" {
   name        = "gcf-matches-create-trigger"
   description = "gcf-matches-create-trigger"
-  schedule    = "0 */3 * * *" # every 3 hours
+  schedule    = "0 */3 1 1 *" # every 3 hours
 
   pubsub_target {
     topic_name = module.gcf_matches-create.pubsub_topic_id
@@ -12,7 +12,7 @@ resource "google_cloud_scheduler_job" "gcf-matches-create-trigger" {
 resource "google_cloud_scheduler_job" "gcf_matches-create-match-sealed-notifications-trigger" {
   name        = "gcf_matches-create-match-sealed-notifications-trigger"
   description = "gcf_matches-create-match-sealed-notifications-trigger"
-  schedule    = "40 */3 * * *" # every 3 hours
+  schedule    = "40 */3 1 1 *" # every 3 hours
 
   pubsub_target {
     topic_name = module.gcf_matches-create-match-sealed-notifications.pubsub_topic_id
@@ -23,7 +23,7 @@ resource "google_cloud_scheduler_job" "gcf_matches-create-match-sealed-notificat
 resource "google_cloud_scheduler_job" "gcf_matches-create-offering-notifications-trigger" {
   name        = "gcf_matches-create-offering-notifications-trigger"
   description = "gcf_matches-create-offering-notifications-trigger"
-  schedule    = "20 */3 * * *" # every 3 hours
+  schedule    = "20 */3 1 1 *" # every 3 hours
 
   pubsub_target {
     topic_name = module.gcf_matches-create-offering-notifications.pubsub_topic_id
@@ -34,7 +34,7 @@ resource "google_cloud_scheduler_job" "gcf_matches-create-offering-notifications
 resource "google_cloud_scheduler_job" "gcf_matches-process-timeout-trigger" {
   name        = "gcf_matches-process-timeout-trigger"
   description = "gcf_matches-process-timeout-trigger"
-  schedule    = "20 */5 * * *" # every 3 hours
+  schedule    = "20 */5 1 1 *" # every 3 hours
 
   pubsub_target {
     topic_name = module.gcf_matches_process_timeout.pubsub_topic_id
@@ -45,7 +45,7 @@ resource "google_cloud_scheduler_job" "gcf_matches-process-timeout-trigger" {
 resource "google_cloud_scheduler_job" "gcf_matches-process-rejections-trigger" {
   name        = "gcf_matches-process-rejections-trigger"
   description = "gcf_matches-process-rejections-trigger"
-  schedule    = "20 */5 * * *" # every 3 hours
+  schedule    = "20 */5 1 1 *" # every 3 hours
 
   pubsub_target {
     topic_name = module.gcf_matches_process_rejections.pubsub_topic_id
