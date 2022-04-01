ca8510393d4015e716895f2e83f66970744e16ec
Piotr Wozniak <piotr.wozniak@ext.ochk.pl>
2022-04-01 22:32:48 +0200

    Use fnc-generated uuid in acc/hosts/guests insert/update

--- hosts-insert/main.py
+++ hosts-insert/main.py
@@ -6,6 +6,7 @@ import time
 from enum import Enum
 from sqlalchemy import create_engine, Table, MetaData, Column, inspect
 from sqlalchemy.dialects.postgresql import *
+import uuid #FIXME Assign db_accounts_id on db level
 
 from google.cloud import secretmanager
 
@@ -132,6 +133,7 @@ def postgres_insert(db_pool, pubsub_msg):
     column_names = {c.name for c in tbl_hosts.columns}
     empty_dict = dict.fromkeys(column_names, None)
     payload = nvl(empty_dict | pubsub_msg)
+    payload['db_hosts_id'] = str(uuid.uuid1()) #FIXME Assign db_accounts_id on db level
 
     stmt = tbl_hosts.insert()
 
